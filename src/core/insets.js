a6b7f9253171172984ad147c41128e5f4ac76899
Joe Davies <jwd101@gmail.com>
2026-01-20 18:30:59 +0100

    eui21 insets

--- src/core/insets.js
+++ src/core/insets.js
@@ -9,16 +9,19 @@ export const buildInsets = function (out, withCenterPoints, mapType) {
         out.insetBoxPosition_ = [out.width_ - out.insetBoxWidth_ - 2 * out.insetBoxPadding_, 2 * out.insetBoxPadding_]
     }
 
-
     // add container to drawing group
-    // Cannot read properties of undefined (reading 'svgId')
     let svg = select('#' + out.svgId_)
     let drawingGroup = svg.select('#em-drawing-' + out.svgId_)
-    const insetsGroup = drawingGroup
-        .append('g')
-        .attr('id', 'em-insets-group')
-        .attr('class', 'em-insets')
-        .attr('transform', 'translate(' + out.insetBoxPosition_[0] + ',' + out.insetBoxPosition_[1] + ')')
+
+    // Check if insets group already exists (e.g., user-defined in Angular template)
+    let insetsGroup = drawingGroup.select('#em-insets-group')
+    if (insetsGroup.empty()) {
+        insetsGroup = drawingGroup
+            .append('g')
+            .attr('id', 'em-insets-group')
+            .attr('class', 'em-insets')
+            .attr('transform', 'translate(' + out.insetBoxPosition_[0] + ',' + out.insetBoxPosition_[1] + ')')
+    }
 
     if (out.insets_ === 'default') {
         //if needed, use default inset config
@@ -30,10 +33,11 @@ export const buildInsets = function (out, withCenterPoints, mapType) {
         const config = out.insets_[i]
         config.svgId = config.svgId || 'inset' + config.geo + Math.random().toString(36).substring(7)
 
-        //get svg element.
-        let svg = select('#' + config.svgId)
-        if (svg.size() == 0) {
-            // Create it as an embeded SVG if it does not exist
+        // Look for existing element with this ID
+        let existingElement = select('#' + config.svgId)
+
+        if (existingElement.size() == 0) {
+            // No element exists - create the full structure
             const x = config.x == undefined ? out.insetBoxPadding_ : config.x
             const y = config.y == undefined ? out.insetBoxPadding_ + i * (out.insetBoxPadding_ + out.insetBoxWidth_) : config.y
             const ggeo = insetsGroup
@@ -42,6 +46,31 @@ export const buildInsets = function (out, withCenterPoints, mapType) {
                 .attr('class', 'em-inset')
                 .attr('transform', 'translate(' + x + ',' + y + ')')
             ggeo.append('svg').attr('id', config.svgId)
+        } else {
+            // Element exists - check what type it is
+            const tagName = existingElement.node().tagName.toLowerCase()
+
+            if (tagName === 'g') {
+                // It's a <g> element - append an <svg> inside it for eurostat-map to use
+                const innerSvgId = config.svgId + '-svg'
+                let innerSvg = existingElement.select('#' + innerSvgId)
+
+                if (innerSvg.empty()) {
+                    // Create inner SVG
+                    existingElement
+                        .append('svg')
+                        .attr('id', innerSvgId)
+                        .attr('class', 'em-inset-svg')
+                        .attr('width', config.width || out.insetBoxWidth_)
+                        .attr('height', config.height || out.insetBoxWidth_)
+                        .attr('overflow', 'hidden')
+                }
+
+                // Update config to point to the inner SVG
+                config._originalSvgId = config.svgId // Keep reference to wrapper
+                config.svgId = innerSvgId
+            }
+            // If it's already an <svg>, use it as-is (existing behavior)
         }
 
         // build inset
@@ -63,13 +92,6 @@ export const buildInsets = function (out, withCenterPoints, mapType) {
 
 /** Build template for inset, based on main one */
 const buildInset = function (config, out, withCenterPoints, mapType) {
-    //TODO find a better way to do that
-
-    //copy map
-    //for(let key__ in map) {
-    //mt[key__] = map[key__];
-    //}
-
     const mt = mapTemplate(config, withCenterPoints, mapType)
 
     //define default values for inset configs
@@ -85,35 +107,35 @@ const buildInset = function (config, out, withCenterPoints, mapType) {
     config.insetTemplates = config.insetTemplates || {}
     config.callback = config.callback || undefined
 
-        //copy main map attributes
-        ;[
-            'nutsLevel_',
-            'nutsYear_',
-            'hoverColor_',
-            //'nutsbnStroke_', // DEPRECATED
-            // 'nutsbnStrokeWidth_', // DEPRECATED
-            'cntrgFillStyle_', // DEPRECATED
-            'cntbnStroke_', // DEPRECATED
-            'cntbnStrokeWidth_', // DEPRECATED
-            'seaFillStyle_', // DEPRECATED
-            'drawCoastalMargin_',
-            'coastalMarginColor_', // DEPRECATED
-            'coastalMarginWidth_', // DEPRECATED
-            'coastalMarginStdDev_',
-            'graticuleStroke_', // DEPRECATED
-            'graticuleStrokeWidth_', // DEPRECATED
-            'lg_',
-            'projectionFunction_',
-            'filterGeometriesFunction_',
-            'processCentroids_',
-        ].forEach(function (att) {
-            mt[att] = out[att]
-        })
+    //copy main map attributes
+    ;[
+        'nutsLevel_',
+        'nutsYear_',
+        'hoverColor_',
+        //'nutsbnStroke_', // DEPRECATED
+        // 'nutsbnStrokeWidth_', // DEPRECATED
+        'cntrgFillStyle_', // DEPRECATED
+        'cntbnStroke_', // DEPRECATED
+        'cntbnStrokeWidth_', // DEPRECATED
+        'seaFillStyle_', // DEPRECATED
+        'drawCoastalMargin_',
+        'coastalMarginColor_', // DEPRECATED
+        'coastalMarginWidth_', // DEPRECATED
+        'coastalMarginStdDev_',
+        'graticuleStroke_', // DEPRECATED
+        'graticuleStrokeWidth_', // DEPRECATED
+        'lg_',
+        'projectionFunction_',
+        'filterGeometriesFunction_',
+        'processCentroids_',
+    ].forEach(function (att) {
+        mt[att] = out[att]
+    })
 
-        //copy stat map attributes/methods
-        ;['stat', 'statData', 'legend', 'legendObj', 'noDataText', 'language', 'transitionDuration', 'tooltip_', 'classToText_'].forEach(function (att) {
-            mt[att] = out[att]
-        })
+    //copy stat map attributes/methods
+    ;['stat', 'statData', 'legend', 'legendObj', 'noDataText', 'language', 'transitionDuration', 'tooltip_', 'classToText_'].forEach(function (att) {
+        mt[att] = out[att]
+    })
 
     //apply config values for inset
     for (let key in config) mt[key + '_'] = config[key]
@@ -128,12 +150,30 @@ const buildInset = function (config, out, withCenterPoints, mapType) {
 export const removeInsets = function (out) {
     if (out.insetTemplates_) {
         for (let template in out.insetTemplates_) {
-            let id = out.insetTemplates_[template].svgId_
-            let existing = select('#' + id)
-            // if (existing) existing.remove()
-            if (existing) existing.html('') // empty them, but dont remove them.
+            const insetTemplate = out.insetTemplates_[template]
+
+            // Handle array of insets (GISCO-2676 case)
+            if (Array.isArray(insetTemplate)) {
+                insetTemplate.forEach((t) => clearInsetSvg(t))
+            } else {
+                clearInsetSvg(insetTemplate)
+            }
         }
-        out.insetTemplates_ = {} //  GISCO-2676
+        out.insetTemplates_ = {}
+    }
+}
+
+/**
+ * Clear the contents of an inset SVG without removing the wrapper
+ */
+const clearInsetSvg = function (insetTemplate) {
+    if (!insetTemplate) return
+
+    let id = insetTemplate.svgId_
+    let existing = select('#' + id)
+
+    if (existing.size() > 0) {
+        existing.html('') // empty contents, but don't remove the element
     }
 }
 
@@ -147,61 +187,14 @@ const defaultInsetConfig = function (s, p) {
         { geo: 'IC', x: 0, y: 0, width: s, height: 0.3 * s },
         { geo: 'CARIB', x: 0, y: 0.3 * s + p, width: 0.5 * s, height: s },
         { geo: 'GF', x: 0.5 * s, y: 0.3 * s + p, width: 0.5 * s, height: 0.75 * s },
-        {
-            geo: 'YT',
-            x: 0.5 * s,
-            y: 1.05 * s + p,
-            width: 0.25 * s,
-            height: 0.25 * s,
-        },
-        {
-            geo: 'RE',
-            x: 0.75 * s,
-            y: 1.05 * s + p,
-            width: 0.25 * s,
-            height: 0.25 * s,
-        },
-        {
-            geo: 'PT20',
-            x: 0,
-            y: 1.3 * s + 2 * p,
-            width: 0.75 * s,
-            height: 0.25 * s,
-        },
-        {
-            geo: 'PT30',
-            x: 0.75 * s,
-            y: 1.3 * s + 2 * p,
-            width: 0.25 * s,
-            height: 0.25 * s,
-        },
+        { geo: 'YT', x: 0.5 * s, y: 1.05 * s + p, width: 0.25 * s, height: 0.25 * s },
+        { geo: 'RE', x: 0.75 * s, y: 1.05 * s + p, width: 0.25 * s, height: 0.25 * s },
+        { geo: 'PT20', x: 0, y: 1.3 * s + 2 * p, width: 0.75 * s, height: 0.25 * s },
+        { geo: 'PT30', x: 0.75 * s, y: 1.3 * s + 2 * p, width: 0.25 * s, height: 0.25 * s },
         { geo: 'MT', x: 0, y: 1.55 * s + 3 * p, width: 0.25 * s, height: 0.25 * s },
-        {
-            geo: 'LI',
-            x: 0.25 * s,
-            y: 1.55 * s + 3 * p,
-            width: 0.25 * s,
-            height: 0.25 * s,
-        },
-        {
-            geo: 'SJ_SV',
-            x: 0.5 * s,
-            y: 1.55 * s + 3 * p,
-            width: 0.25 * s,
-            height: 0.25 * s,
-        },
-        {
-            geo: 'SJ_JM',
-            x: 0.75 * s,
-            y: 1.55 * s + 3 * p,
-            width: 0.25 * s,
-            height: 0.25 * s,
-        },
-        /*{geo:"IC", x:0, y:0}, {geo:"RE", x:dd, y:0}, {geo:"YT", x:2*dd, y:0},
-        {geo:"GP", x:0, y:dd}, {geo:"MQ", x:dd, y:dd}, {geo:"GF",scale:"10M", x:2*dd, y:dd},
-        {geo:"PT20", x:0, y:2*dd}, {geo:"PT30", x:dd, y:2*dd}, {geo:"MT", x:2*dd, y:2*dd},
-        {geo:"LI",scale:"01M", x:0, y:3*dd}, {geo:"SJ_SV", x:dd, y:3*dd}, {geo:"SJ_JM",scale:"01M", x:2*dd, y:3*dd},*/
-        //{geo:"CARIB", x:0, y:330}, {geo:"IS", x:dd, y:330}
+        { geo: 'LI', x: 0.25 * s, y: 1.55 * s + 3 * p, width: 0.25 * s, height: 0.25 * s },
+        { geo: 'SJ_SV', x: 0.5 * s, y: 1.55 * s + 3 * p, width: 0.25 * s, height: 0.25 * s },
+        { geo: 'SJ_JM', x: 0.75 * s, y: 1.55 * s + 3 * p, width: 0.25 * s, height: 0.25 * s },
     ]
     //hide graticule for insets
     for (let i = 0; i < out.length; i++) out[i].drawGraticule = false
