e1273039e0a62f10e329a00833078d079a6f60a8
Joe Davies <jwd101@gmail.com>
2026-02-16 13:05:00 +0100

    square stamps

--- src/core/stamps.js
+++ src/core/stamps.js
@@ -64,3 +64,68 @@ export const appendStamp = (stampConfig, map) => {
         }
     }
 }
+
+// Splitting by both spaces and pilcrows
+const getWords = (text) => {
+    return text
+        .split(/(?<=¶)|\s+/g)
+        .map((word) => word.trim())
+        .filter((word) => word.length > 0)
+}
+
+// Computes text width
+const measureWidth = (text) => {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+
+    textElement.textContent = text
+    svg.appendChild(textElement)
+    document.body.appendChild(svg)
+
+    const width = textElement.getComputedTextLength()
+    document.body.removeChild(svg)
+
+    return width + 10
+}
+
+// Compute text radius
+const getTextRadius = (lines, lineHeight) => {
+    let radius = 0
+    for (let i = 0, n = lines.length; i < n; ++i) {
+        const dy = (Math.abs(i - n / 2 + 0.5) + 0.5) * lineHeight
+        const dx = lines[i].width / 2
+        radius = Math.max(radius, Math.sqrt(dx ** 2 + dy ** 2))
+    }
+    return radius
+}
+
+// Handles forced line breaks
+const getLines = (words, targetWidth) => {
+    let lines = []
+    let line = { width: 0, text: '' }
+
+    for (let i = 0, n = words.length; i < n; ++i) {
+        if (words[i] === '¶') {
+            // Push current line (if it has text)
+            if (line.text) lines.push(line)
+            // Start a new empty line
+            line = { width: 0, text: '' }
+            continue
+        }
+
+        let lineText1 = (line.text ? line.text + ' ' : '') + words[i]
+        let lineWidth1 = measureWidth(lineText1)
+
+        if ((line.width + lineWidth1) / 2 < targetWidth) {
+            line.width = lineWidth1
+            line.text = lineText1
+        } else {
+            lines.push(line)
+            line = { width: measureWidth(words[i]), text: words[i] }
+        }
+    }
+
+    if (line.text) lines.push(line) // Push last line if it exists
+
+    return lines
+}
